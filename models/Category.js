5ce689daaeb4662cc2b44c9fce5a0bd13c08327b
just1nt4n <stegenkewl@gmail.com>
2026-01-12 14:43:24 +0800

    Revert "Implementation of Paypal and Credit/Debit Card payment."
    This reverts commit 4fdcf101fa6824655b8399da9f515fc02d07a066.

--- models/Category.js
+++
@@ -1,7 +0,0 @@
-// Category model built on top of existing helpers/structures.
-
-module.exports = {
-  getAllFromMemory(categoriesStore) {
-    return (categoriesStore || []).slice();
-  }
-};
--- models/Membership.js
+++
@@ -1,7 +0,0 @@
-// Simple membership model focused on points and tiers.
-
-module.exports = {
-  getPointsFromUser(user) {
-    return Number(user && user.points != null ? user.points : 0) || 0;
-  }
-};
